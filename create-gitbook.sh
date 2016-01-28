d2194e3a2d69e036e934124cf89b9185f14d5c3c
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-01-28 10:41:36 -0500

    update

--- create-gitbook.sh
+++ create-gitbook.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 ls
 ls run-compliance-masonry
+ls run-compliance-masonry/compliance-repository
+ls run-compliance-masonry/compliance-repository/exports
 #npm install gitbook-cli -g
 #gitbook build
