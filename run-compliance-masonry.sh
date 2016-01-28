9b91cdf1f946780b206cba8eea42322bf38f3d7f
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-01-28 14:42:48 -0500

    update

--- run-compliance-masonry.sh
+++ run-compliance-masonry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #pip install git+https://github.com/opencontrol/compliance-masonry.git
-#cd compliance-repository
+cd compliance-repository
 masonry certs LATO
 masonry docs gitbook LATO
 #exit 0
