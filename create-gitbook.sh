d59273c89b98d2f8d0eeea89f8e6e53e1eea1c6b
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-01-28 17:50:31 -0500

    deployment update

--- create-gitbook.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd run-compliance-masonry/compliance-repository/exports/gitbook/
-gitbook build
--- run-compliance-masonry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#pip install git+https://github.com/opencontrol/compliance-masonry.git
-cd compliance-repository
-masonry certs LATO
-masonry docs gitbook LATO
-#exit 0
